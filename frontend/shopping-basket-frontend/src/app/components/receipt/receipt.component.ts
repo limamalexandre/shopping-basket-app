a3ab5d3cd1cf9ff147cac48b7241574387e45328
Alexandre Lima <limamalexandre@gmail.com>
2025-03-20 20:31:29 +0000

    create receipt component

---
+++ frontend/shopping-basket-frontend/src/app/components/receipt/receipt.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ReceiptService } from '../../services/receipt.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-receipt',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './receipt.component.html',
+  styleUrl: './receipt.component.scss'
+})
+export class ReceiptComponent {
+  receipt: any;
+
+  constructor(
+    private receiptService: ReceiptService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    // Retrieve the receipt from the shared service.
+    this.receipt = this.receiptService.receipt;
+    if (!this.receipt) {
+      // If no receipt exists, redirect back to the basket.
+      this.router.navigate(['/']);
+    }
+  }
+}
