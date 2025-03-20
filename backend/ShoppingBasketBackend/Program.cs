d0592ba5083e874229031c01d3c22ed54350c424
Alexandre Lima <limamalexandre@gmail.com>
2025-03-20 23:03:01 +0000

    Add cors and register services and repositories

--- backend/ShoppingBasketBackend/Program.cs
+++ backend/ShoppingBasketBackend/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ShoppingBasketBackend.Data;
+using ShoppingBasketBackend.Models;
 using ShoppingBasketBackend.Repositories;
 using ShoppingBasketBackend.Services;
 
@@ -12,8 +13,26 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
                      new MySqlServerVersion(new Version(8, 0, 21))));
 
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddScoped<IDiscount, ApplesDiscount>();
+builder.Services.AddScoped<IDiscount, MultiBuyDiscount>();
+
 // Configure Services and Repositories
 builder.Services.AddScoped<IBasketService, BasketService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -27,6 +46,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowSpecificOrigins");
 
 app.UseHttpsRedirection();
 
